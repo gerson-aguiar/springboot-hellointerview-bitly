96f6daffa7f2ae7635681fbbea7634c3285dc6bd
Gerson Aguiar <gersoncafilho@gmail.com>
2026-02-17 12:55:06 -0300

    commmit - v0.0.3

--- src/main/java/com/spring/bitly/core/controller/ShortUrlResponseDTO.java
+++ src/main/java/com/spring/bitly/core/controller/ShortUrlResponseDTO.java
@@ -11,7 +11,7 @@ public record ShortUrlResponseDTO(
         return new ShortUrlResponseDTO(
                 entity.getOriginalUrl(),
                 entity.getCode(),
-                "v0.0.2"
+                "v0.0.3"
         );
     }
 }
